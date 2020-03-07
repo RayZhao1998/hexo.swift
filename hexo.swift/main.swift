927f3fd59df854db4135216e12653d720487e6ef
RayZhao <ninjiacoder@gmail.com>
2020-03-08 01:53:59 +0800

    Initial Commit

---
+++ hexo.swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hexo.swift
+//
+//  Created by Ziyuan Zhao on 2020/3/8.
+//  Copyright © 2020 Ziyuan Zhao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
